68867083229dc15f96a7f7560f266f92794a0a1b
Niket Sheth <niketsheth31@gmail.com>
2025-09-13 17:10:50 -0500

    Made site for dev day

--- src/app/components/coursecard.js
+++ src/app/components/coursecard.js
@@ -9,10 +9,15 @@ const data = [
   }
 ];
 
-const CourseCard = () => {
+const CourseCard = (props) => {
   return (
-    <div>
-      
+    <div class="container grid grid-col-1 grid-rows-1 gap-4 rounded-lg mx-auto py-8 border-2 border-solid border-gray-300 bg-white shadow-lg p-6 md:grid-cols-3 md:grid-rows-1">
+      <div class="col-span-2">
+        <h1 className="font-bold text-blue-800 text-2xl">{props.courseCode}</h1>
+        <h3 className="font-bold text-black">{props.title}</h3>
+        <h4 className="text-gray-500">{props.description}</h4>
+        <span className="text-green-600 rounded-xl border-5 border-solid border-lime-300 bg-lime-300">Credit Hours: {props.creditHours}</span>
+      </div>
     </div>
   );
 };
--- src/app/components/navbar.js
+++ src/app/components/navbar.js
@@ -1,12 +1,26 @@
 import React from "react";
 import Link from "next/link";
-import { User, Settings } from "lucide-react";
+import { User, Settings, Pi } from "lucide-react";
 
 function Navbar() {
     return (
-        <div>
-            
-        </div>
+        <nav className="flex justify-between items-center py-4 px-8 bg-gray-100 border-b border-gray-300 text-black">
+            <div className="flex gap-4">
+                <Link href="/settings" className="group relative">
+                    <Settings size={24} className="cursor-pointer"/>
+                </Link>
+            </div>
+            <div className="font-bold text-3xl">
+                <Link href="/" className="group relative">
+                    <span className="cursor-pointer">DevGPA</span>
+                </Link>
+            </div>
+            <div className="flex gap-4">
+                <Link href="/profile" className="group relative">
+                    <User size={24} className="cursor-pointer"/>
+                </Link>
+            </div>
+        </nav>
     );
 }
 
